4dc826dc058d839997bb3d37a401565145e55ee1
ueaj <dev.sel20@gmail.com>
2023-05-05 21:52:23 -0500

    better prefetching (way faster now)

--- src/load.py
+++ src/load.py
@@ -9,11 +9,10 @@ import joblib
 
 threads = os.cpu_count() or 1
 
-def prefetch_iterable(values, size=1):
+def prefetch_iterable(values, size=2):
 	queue = collections.deque()
 
 	iterator = iter(values)
-
 	def enqueue(n):
 		for value in itertools.islice(iterator, n):
 			queue.append([jax.device_put(value)])
@@ -21,10 +20,9 @@ def prefetch_iterable(values, size=1):
 	enqueue(size)
 	while queue:
 		ret = queue.popleft()[0]
+		enqueue(1)
 		yield ret
 		jax.device_get(ret)
-		enqueue(1)
-
 
 def normalize(entry):
 	return {'label': entry['label'], 'image': entry['image'].reshape(28, 28, 1) / 255.0}
@@ -53,6 +51,9 @@ def get_datasets(num_epochs, batch_size):
 	test_ds = test_ds.map(make_batch, num_proc=threads, batched=True, batch_size=batch_size)
 
 	print(f"{train_ds.shape}")
+
+	# train_ds = [train_ds.shard(num_shards=num_epochs, index=i) for i in range(num_epochs)]
+	# test_ds = [test_ds.shard(num_shards=num_epochs, index=i) for i in range(num_epochs)]
 
 	start = time.time()
 	train_ds = map_iter([*range(num_epochs)], lambda i: list(train_ds.shard(num_shards=num_epochs, index=i)))
